811dd4e09cc8032acc75ef68a2f2383d8804c27d
Soumik-CICD <60919299+Soumik-CICD@users.noreply.github.com>
2023-09-05 21:33:08 +0530

    Update format.tf

--- format.tf
+++ format.tf
@@ -1,1 +1,10 @@
+provider "aws" {
+  region = "us-east-1"
+  access_key = "AKIASQ5X2JYSVS6GIYOU"
+  secret_key = "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"
+}
 
+resource "aws_instance" "ec2" {
+    ami = "ami-051f7e7f6c2f40dc1"
+    instance_type = "t2.large"
+}
